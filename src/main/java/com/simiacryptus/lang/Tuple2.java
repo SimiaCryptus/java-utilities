95e78b47ac641f9177978a35c1162f4402bb5edd
Andrew Charneski <acharneski@gmail.com>
2019-06-24 08:16:14 -0700

    wip

--- src/main/java/com/simiacryptus/lang/Tuple2.java
+++ src/main/java/com/simiacryptus/lang/Tuple2.java
@@ -19,9 +19,15 @@
 
 package com.simiacryptus.lang;
 
-public class Tuple2<A, B> {
+import java.io.Serializable;
+
+public class Tuple2<A, B> implements Serializable {
   public final A _1;
   public final B _2;
+
+  public Tuple2() {
+    this(null,null);
+  }
 
   public Tuple2(final A a, final B b) {
     _1 = a;
